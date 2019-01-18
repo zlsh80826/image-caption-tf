240688f9b119609ca6cb116b4c821e482df06e55
zlsh80826 <zlsh80826@gmail.com>
2019-01-18 11:28:54 +0800

    update lightweight configuration

--- script/config.py
+++ script/config.py
@@ -2,20 +2,22 @@ import tensorflow as tf
 import pickle
 
 feature_dim = 4032
-decode_map = pickle.load(open('../data/captions/dec_map.pkl', 'rb'))
+decode_map = pickle.load(open('../data/dec_map.pkl', 'rb'))
 vocab_dim = len(decode_map)
 
 def get_hparams():
     hparams = tf.contrib.training.HParams(
-        feature_dim = feature_dim,
-        image_embedding_dim=2048,
+        feature_dim=feature_dim,
+        image_embedding_dim=1536,
         word_embedding_dim=512,
         vocab_dim=vocab_dim,
         training_epochs=20,
         max_caption_len=16, 
-        hidden_dim=2048,
+        hidden_dim=1024,
         batch_size=64,
         num_layers=1,
+        queue_size=3,
+        beam_size=3,
         dropout=0.5,
         lr=1e-4,
         ckpt_dir='../model/')
