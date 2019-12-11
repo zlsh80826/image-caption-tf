ccf56fafceefde8dedd31c457dd2c3c45b368b56
zlsh80826 <zlsh80826@gmail.com>
2019-12-11 18:46:35 +0800

    add wget comment

--- data/download.sh
+++ data/download.sh
@@ -4,6 +4,11 @@ set -xe
 aria2c -x 16 'http://images.cocodataset.org/zips/train2014.zip'
 aria2c -x 16 'http://images.cocodataset.org/zips/val2014.zip'
 
+# if you don't have aria2c, please use following command to download the data
+# aria2 is a tool that can perform parallel download
+# wget 'http://images.cocodataset.org/zips/train2014.zip'
+# wget 'http://images.cocodataset.org/zips/val2014.zip'
+
 unzip train2014.zip
 unzip val2014.zip
 
